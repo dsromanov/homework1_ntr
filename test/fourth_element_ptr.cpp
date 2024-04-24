4eb2c79aa2c34e8ca20e38eaf6c911f9e91b4327
Dmitrii Romanov <dromanov2002@mail.ru>
2024-04-24 23:10:53 +0300

    fixed missed string)

--- test/fourth_element_ptr.cpp
+++ test/fourth_element_ptr.cpp
@@ -5,6 +5,7 @@ int main() {
     for (int i = 0; i < 10; ++i) {
         arr[i] = i * i;
     }
+    int *ptr = &arr[0];
     int fourthElement = *(ptr + 3);
     std::cout << "Четвертый элемент массива: " << fourthElement << std::endl;
     return 0;
